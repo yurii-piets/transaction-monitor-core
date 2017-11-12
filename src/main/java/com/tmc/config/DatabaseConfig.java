3548170b925f5cdc4ebfdf5f4da551f3ea4b4334
Yurii Piets <yurii.piets@gmail.com>
2017-11-12 22:40:21 +0100

    qualified beans

--- src/main/java/com/tmc/config/DatabaseConfig.java
+++ src/main/java/com/tmc/config/DatabaseConfig.java
@@ -1,20 +1,27 @@
 package com.tmc.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
 
-    @Primary
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "tmone")
+    @Qualifier("tmone")
     public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "tmtwo")
+    @Qualifier("tmtwo")
+    public DataSource dataSource2() {
         return DataSourceBuilder.create().build();
     }
 }
--- src/main/java/com/tmc/data/DataFrom.java
+++ src/main/java/com/tmc/data/DataFrom.java
@@ -1,23 +1,19 @@
 package com.tmc.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@Service
 @Component
 public class DataFrom {
 
-    private final DataSource dataSource;
-
     @Autowired
-    public DataFrom(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    @Qualifier("tmone")
+    private DataSource dataSource;
 
     public Connection dbConnect() throws SQLException {
         return dataSource.getConnection();
