2b683e80eef1a95f93d4333a4901ff5bafbe68fd
Thuleq <kszczyrbak8@gmail.com>
2017-11-17 14:04:34 +0100

    branch fix

--- src/main/java/com/tmc/transaction/factory/annotation/Transactional.java
+++
@@ -1,6 +0,0 @@
-package com.tmc.transaction.factory.annotation;
-
-@Target(ElementType.TYPE) @Retention(RetentionPolicy.CLASS)
-public @interface Transactional {
-
-}
--- src/main/java/com/tmc/transaction/factory/annotation/TransactionalProcessor.java
+++
@@ -1,6 +0,0 @@
-package com.tmc.transaction.factory.annotation;
-
-@AutoService(Processor.class)
-public class TransactionalProcessor extends AbstractProcessor {
-
-}
