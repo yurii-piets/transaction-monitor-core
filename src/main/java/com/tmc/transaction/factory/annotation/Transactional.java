e74cf8ada14c6a9f594db79e52a91cef1045395d
Thuleq <kszczyrbak8@gmail.com>
2017-11-17 14:01:07 +0100

    initial annotation declaration files

---
+++ src/main/java/com/tmc/transaction/factory/annotation/Transactional.java
@@ -0,0 +1,6 @@
+package com.tmc.transaction.factory.annotation;
+
+@Target(ElementType.TYPE) @Retention(RetentionPolicy.CLASS)
+public @interface Transactional {
+
+}
---
+++ src/main/java/com/tmc/transaction/factory/annotation/TransactionalProcessor.java
@@ -0,0 +1,6 @@
+package com.tmc.transaction.factory.annotation;
+
+@AutoService(Processor.class)
+public class TransactionalProcessor extends AbstractProcessor {
+
+}
