c4a0c3980dcc8bb035cd9c6e0363ca8aa32b49ad
Yurii Piets <yurii.piets@gmail.com>
2018-01-31 19:30:25 +0100

    removed redundant sql files

--- src/test/resources/sql/commit_db1.sql
+++
@@ -1,8 +0,0 @@
-insert into klienci values
-                    (16, 'Pariusz Dalka', 'Krakow', '666 666 666'),
-                    (23, 'Rollback Successful', 'PSQL', '010 001 100');
-
-update klienci set nazwa='Lech Balcerowicz' where miejscowosc='Warszawa';
-
-update zamowienia set opis='update successful';
-insert into zamowienia values (16, 15, 'insert successful');
